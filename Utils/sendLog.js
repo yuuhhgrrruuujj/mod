5db2c229b9842e9cb1bb0a77987b7b597b7ac62a
yuuhhgrrruuujj <110064383+yuuhhgrrruuujj@users.noreply.github.com>
2025-11-08 23:29:51 -0500

    Update sendLog.js

--- Utils/sendLog.js
+++ Utils/sendLog.js
@@ -1,28 +1,14 @@
-const db = require('./db');
+const Keyv = require('keyv');
+const logChannels = new Keyv(process.env.logChannels);
 
 module.exports = {
-  db, // export db so other modules can use it
-
   sendLog: async (interaction, message) => {
-    try {
-      const logChName = await db.get(`logchannel_${interaction.guild.id}`);
-      const logChannel = interaction.guild.channels.cache.find(ch => ch.name === logChName);
-
-      const msgContent = message.color ? { embeds: [message] } : { content: message };
-
-      await interaction.reply(msgContent).catch(() => null);
-      if (logChannel) await logChannel.send(msgContent).catch(() => null);
-    } catch (err) {
-      console.error('Error in sendLog:', err);
-    }
-  },
-
-  setLogChannel: async (guild, channelName) => {
-    try {
-      await db.set(`logchannel_${guild.id}`, channelName);
-      console.log(`Log channel for guild ${guild.name} set to ${channelName}`);
-    } catch (err) {
-      console.error('Error setting log channel:', err);
-    }
+    const logChName = await logChannels.get(`logchannel_${interaction.guild.id}`);
+    const log = await interaction.guild.channels.cache.find((ch) => ch.name === `${logChName}`);
+    let msgContent = {};
+    if (message.color) msgContent = { embeds: [message] };
+    else msgContent = { content: message };
+    interaction.reply(msgContent);
+    if (log) log.send(msgContent);
   }
-};
+}
