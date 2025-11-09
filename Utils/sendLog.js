681ebaa056c3221c433d98b54d09fe680623a97a
yuuhhgrrruuujj <110064383+yuuhhgrrruuujj@users.noreply.github.com>
2025-11-08 22:50:31 -0500

    Update sendLog.js

--- Utils/sendLog.js
+++ Utils/sendLog.js
@@ -1,14 +1,30 @@
 const Keyv = require('keyv');
-const logChannels = new Keyv(process.env.const logChannels = new Keyv(process.env.DATABASE_URL););
+
+// Make sure the environment variable exists
+if (!process.env.DATABASE_URL) {
+  console.error('Error: DATABASE_URL is not defined!');
+  process.exit(1);
+}
+
+// Create Keyv instance
+const logChannels = new Keyv(process.env.DATABASE_URL);
+
+// Optional: listen for connection errors
+logChannels.on('error', err => console.error('Keyv connection error:', err));
 
 module.exports = {
   sendLog: async (interaction, message) => {
     const logChName = await logChannels.get(`logchannel_${interaction.guild.id}`);
-    const log = await interaction.guild.channels.cache.find((ch) => ch.name === `${logChName}`);
+    const log = interaction.guild.channels.cache.find(ch => ch.name === logChName);
+
     let msgContent = {};
     if (message.color) msgContent = { embeds: [message] };
     else msgContent = { content: message };
-    interaction.reply(msgContent);
+
+    // Reply to interaction
+    await interaction.reply(msgContent);
+
+    // Send to log channel if it exists
     if (log) log.send(msgContent);
   }
-}
+};
